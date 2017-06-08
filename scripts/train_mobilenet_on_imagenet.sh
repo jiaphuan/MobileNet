bbff7540ef35d6dc5a3fda7f0bdeaaa71ac0efa1
jiaphuan@microsoft.com <jiaphuan@microsoft.com>
2017-06-08 01:40:18 -0700

    lr decay

--- scripts/train_mobilenet_on_imagenet.sh
+++ scripts/train_mobilenet_on_imagenet.sh
@@ -8,13 +8,13 @@
 # ./scripts/train_mobilenet_on_imagenet.sh
 
 # Where the checkpoint and logs will be saved to.
-TRAIN_DIR=/home/jiaphuan/exp/mobilenet
+TRAIN_DIR=/home/jiaphuan/exp/mobilenet201706080118
 
 # Where the dataset is saved to.
 DATASET_DIR=/home/linjuny/dataset/imagenet2012/
 
 # Run training.
-python train_image_classifier.py \
+CUDA_VISIBLE_DEVICES=2 python train_image_classifier.py \
   --train_dir=${TRAIN_DIR} \
   --dataset_name=imagenet \
   --dataset_split_name=train \
@@ -22,8 +22,8 @@ python train_image_classifier.py \
   --model_name=mobilenet \
   --preprocessing_name=mobilenet \
   --width_multiplier=1.0 \
-  --max_number_of_steps=500000 \
-  --batch_size=256 \
+  --max_number_of_steps=1000000 \
+  --batch_size=128 \
   --save_interval_secs=240 \
   --save_summaries_secs=240 \
   --log_every_n_steps=100 \
@@ -32,17 +32,18 @@ python train_image_classifier.py \
   --opt_epsilon=1.0\
   --learning_rate=0.1 \
   --learning_rate_decay_type=polynomial \
+  --end_learning_rate=0. \
   --learning_rate_decay_factor=0.1 \
   --momentum=0.9 \
-  --num_epochs_per_decay=30.0 \
+  --num_epochs_per_decay=100.0 \
   --weight_decay=0.0001 \
   --num_clones=1
 
 # Run evaluation.
-python eval_image_classifier.py \
-  --checkpoint_path=${TRAIN_DIR} \
-  --eval_dir=${TRAIN_DIR} \
-  --dataset_name=imagenet \
-  --dataset_split_name=validation \
-  --dataset_dir=${DATASET_DIR} \
-  --model_name=mobilenet
+#CUDA_VISIBLE_DEVICES='' python eval_image_classifier.py \
+#  --checkpoint_path=${TRAIN_DIR} \
+#  --eval_dir=${TRAIN_DIR} \
+#  --dataset_name=imagenet \
+#  --dataset_split_name=validation \
+#  --dataset_dir=${DATASET_DIR} \
+#  --model_name=mobilenet
